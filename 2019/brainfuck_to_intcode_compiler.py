ad0ce1b205368aed522fb76f6319586d8dcd9fd5
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-01 21:33:10 +0000

    Document and slightly refactor some 2019 stuff

--- 2019/brainfuck_to_intcode_compiler.py
+++ 2019/brainfuck_to_intcode_compiler.py
@@ -1,280 +1,317 @@
+"""
+This is a proof that intcode is still Turing-complete with only immediate adressing (mode 1) allowed for non-output parameters
+"""
+
+from intcode import Machine
 mode_pos = 0
 mode_imm = 1
 mode_rel = 2
-DUMMY=0
+DUMMY = 0
+
 
 def flatten(xss):
-  return [x for xs in xss for x in xs]
+    return [x for xs in xss for x in xs]
+
 
 def opcode(op, mode1=0, mode2=0, mode3=0):
-  return op + 100*mode1 + 1000*mode2 + 10000*mode3
+    return op + 100*mode1 + 1000*mode2 + 10000*mode3
 
-def add(a, b, res, a_mode = 1, b_mode = 1, res_mode = 0):
-  return [opcode(1, a_mode, b_mode, res_mode), a, b, res]
 
-def mul(a, b, res, a_mode = 1, b_mode = 1, res_mode = 0):
-  return [opcode(2, a_mode, b_mode, res_mode), a, b, res]
+def add(a, b, res, a_mode=1, b_mode=1, res_mode=0):
+    return [opcode(1, a_mode, b_mode, res_mode), a, b, res]
+
+
+def mul(a, b, res, a_mode=1, b_mode=1, res_mode=0):
+    return [opcode(2, a_mode, b_mode, res_mode), a, b, res]
+
 
 def inp(res, mode=0):
-  return [opcode(3, mode), res]
+    return [opcode(3, mode), res]
+
 
 def out(a, mode=1):
-  return [opcode(4, mode), a]
+    return [opcode(4, mode), a]
+
 
 def jnz(a, lab, a_mode=1, lab_mode=1):
-  return [opcode(5, a_mode, lab_mode), a, lab]
+    return [opcode(5, a_mode, lab_mode), a, lab]
+
 
 jtrue = jnz
 
+
 def jz(a, lab, a_mode=1, lab_mode=1):
-  return [opcode(6, a_mode, lab_mode), a, lab]
+    return [opcode(6, a_mode, lab_mode), a, lab]
+
 
 jfalse = jz
 
-def tlt(a, b, res, a_mode = 1, b_mode = 1, res_mode = 0):
-  return [opcode(7, a_mode, b_mode, res_mode), a, b, res]
 
-def teq(a, b, res, a_mode = 1, b_mode = 1, res_mode = 0):
-  return [opcode(8, a_mode, b_mode, res_mode), a, b, res]
+def tlt(a, b, res, a_mode=1, b_mode=1, res_mode=0):
+    return [opcode(7, a_mode, b_mode, res_mode), a, b, res]
 
-def add_rel(a, mode = 1):
-  return [opcode(9, mode), a]
 
-def halt(): 
-  return [99]
+def teq(a, b, res, a_mode=1, b_mode=1, res_mode=0):
+    return [opcode(8, a_mode, b_mode, res_mode), a, b, res]
+
+
+def add_rel(a, mode=1):
+    return [opcode(9, mode), a]
+
+
+def halt():
+    return [99]
+
 
 label_idx = 0
+
+
 def fresh_lab(name="lab"):
-  global label_idx
-  label_idx += 1
-  return name + str(label_idx)
+    global label_idx
+    label_idx += 1
+    return name + str(label_idx)
+
 
 def lab_def(lab):
-  return [lab + ":"]
+    return [lab + ":"]
+
 
 def remove_labels(code):
-  labels = {}
-  i = 0
-  for instr in code:
-    if type(instr) == str and instr[-1] == ":":
-      labels[instr[:-1]] = i
-    else:
-      i+=1
-  res = []
-  for instr in code:
-    if type(instr) == str:
-      if not instr[-1] == ":":
-        spl = instr.split("+")
-        res.append(labels[spl[0]] + sum([int(j) for j in spl[1:]]))
-    else:
-      res.append(instr)
-  return res
+    labels = {}
+    i = 0
+    for instr in code:
+        if type(instr) == str and instr[-1] == ":":
+            labels[instr[:-1]] = i
+        else:
+            i += 1
+    res = []
+    for instr in code:
+        if type(instr) == str:
+            if not instr[-1] == ":":
+                spl = instr.split("+")
+                res.append(labels[spl[0]] + sum([int(j) for j in spl[1:]]))
+        else:
+            res.append(instr)
+    return res
+
 
 def register(name, init=0):
-  return flatten([
-    lab_def(name),
-    add(init, 0, DUMMY),
-    jz(0, DUMMY)
-  ])
+    return flatten([
+        lab_def(name),
+        add(init, 0, DUMMY),
+        jz(0, DUMMY)
+    ])
+
 
 def read_reg_to(reg_name, res):
-  lab = fresh_lab("read_"+reg_name)
-  return flatten([
-    add(res, 0, reg_name+"+3"),
-    add(0, lab, reg_name+"+6"),
-    jz(0, reg_name),
-    lab_def(lab)
-  ])  
+    lab = fresh_lab("read_"+reg_name)
+    return flatten([
+        add(res, 0, reg_name+"+3"),
+        add(0, lab, reg_name+"+6"),
+        jz(0, reg_name),
+        lab_def(lab)
+    ])
+
 
 def write_reg(val, reg_name):
-  return add(val, 0, reg_name+"+1")
+    return add(val, 0, reg_name+"+1")
+
 
 def call(sub):
-  lab = fresh_lab(sub)
-  return flatten([
-    add(lab, 0, sub +"_end+2"),
-    jz(0, sub +"_start"),
-    lab_def(lab)
-  ])
+    lab = fresh_lab(sub)
+    return flatten([
+        add(lab, 0, sub + "_end+2"),
+        jz(0, sub + "_start"),
+        lab_def(lab)
+    ])
+
 
 def write_mem_cell(real_defn=False):
-  if real_defn:
-    template = register("")[1:]
-    template[3] = "mem_val+1"
-    res = ["write_mem_cell_start:"]
-    for off, instr in enumerate(template):
-      if off == 1:
-        continue # don't overrite the actual value
-      lab = fresh_lab("write_mem")
-      res += flatten([
-        read_reg_to("mem_ptr", lab+"+1"),
-        lab_def(lab),
-        add(DUMMY, off, lab+"+7"),
-        add(instr, 0, DUMMY)
-      ])
-    return flatten([
-      res,
-      ["write_mem_cell_end:"],
-      jz(0, DUMMY)
-    ])
-  else:
-    return call("write_mem_cell")
+    if real_defn:
+        template = register("")[1:]
+        template[3] = "mem_val+1"
+        res = ["write_mem_cell_start:"]
+        for off, instr in enumerate(template):
+            if off == 1:
+                continue  # don't overrite the actual value
+            lab = fresh_lab("write_mem")
+            res += flatten([
+                read_reg_to("mem_ptr", lab+"+1"),
+                lab_def(lab),
+                add(DUMMY, off, lab+"+7"),
+                add(instr, 0, DUMMY)
+            ])
+        return flatten([
+            res,
+            ["write_mem_cell_end:"],
+            jz(0, DUMMY)
+        ])
+    else:
+        return call("write_mem_cell")
 
 
 def write_mem(real_defn=False):
-  if real_defn:   
-    lab1 = fresh_lab("write_mem")
-    lab2 = fresh_lab("write_mem")
-    return flatten([
-      ["write_mem_start:"],
-      read_reg_to("mem_ptr", lab1+"+1"),
-      lab_def(lab1),
-      add(DUMMY, 1, lab2+"+3"),
-      read_reg_to("mem_val", lab2+"+1"),
-      lab_def(lab2),
-      add(DUMMY, 0, DUMMY),
-      ["write_mem_end:"],
-      jz(0, DUMMY)
-    ])
-    return res
-  else:
-    return call("write_mem")
+    if real_defn:
+        lab1 = fresh_lab("write_mem")
+        lab2 = fresh_lab("write_mem")
+        return flatten([
+            ["write_mem_start:"],
+            read_reg_to("mem_ptr", lab1+"+1"),
+            lab_def(lab1),
+            add(DUMMY, 1, lab2+"+3"),
+            read_reg_to("mem_val", lab2+"+1"),
+            lab_def(lab2),
+            add(DUMMY, 0, DUMMY),
+            ["write_mem_end:"],
+            jz(0, DUMMY)
+        ])
+    else:
+        return call("write_mem")
+
 
 def read_mem(real_defn=False):
-  if real_defn:
-    lab1 = fresh_lab("read_mem")
-    lab2 = fresh_lab("read_mem")
-    lab3 = fresh_lab("read_mem")
-    return flatten([
-      ["read_mem_start:"],
-      read_reg_to("mem_ptr", lab1+"+1"),
-      read_reg_to("mem_ptr", lab2+"+6"),
-      lab_def(lab1),
-      add(DUMMY, 6, lab2+"+3"),
-      lab_def(lab2),
-      add(0, lab3, DUMMY),
-      jz(0, DUMMY),
-      lab_def(lab3),
-      ["read_mem_end:"],
-      jz(0, DUMMY)
-    ])
-  else:
-    return call("read_mem")
+    if real_defn:
+        lab1 = fresh_lab("read_mem")
+        lab2 = fresh_lab("read_mem")
+        lab3 = fresh_lab("read_mem")
+        return flatten([
+            ["read_mem_start:"],
+            read_reg_to("mem_ptr", lab1+"+1"),
+            read_reg_to("mem_ptr", lab2+"+6"),
+            lab_def(lab1),
+            add(DUMMY, 6, lab2+"+3"),
+            lab_def(lab2),
+            add(0, lab3, DUMMY),
+            jz(0, DUMMY),
+            lab_def(lab3),
+            ["read_mem_end:"],
+            jz(0, DUMMY)
+        ])
+    else:
+        return call("read_mem")
 
-    
+
 def bf_incr():
-  lab = fresh_lab("incr")
-  return flatten([
-    read_mem(),
-    read_reg_to("mem_val", lab+"+1"),
-    lab_def(lab),
-    add(DUMMY, 1, lab+"+5"),
-    write_reg(DUMMY, "mem_val"),
-    write_mem()
-  ])  
+    lab = fresh_lab("incr")
+    return flatten([
+        read_mem(),
+        read_reg_to("mem_val", lab+"+1"),
+        lab_def(lab),
+        add(DUMMY, 1, lab+"+5"),
+        write_reg(DUMMY, "mem_val"),
+        write_mem()
+    ])
+
 
 def bf_decr():
-  lab = fresh_lab("decr")
-  return flatten([
-    read_mem(),
-    read_reg_to("mem_val", lab+"+1"),
-    lab_def(lab),
-    add(DUMMY, -1, lab+"+5"),
-    write_reg(DUMMY, "mem_val"),
-    write_mem()
-  ]) 
+    lab = fresh_lab("decr")
+    return flatten([
+        read_mem(),
+        read_reg_to("mem_val", lab+"+1"),
+        lab_def(lab),
+        add(DUMMY, -1, lab+"+5"),
+        write_reg(DUMMY, "mem_val"),
+        write_mem()
+    ])
+
 
 def bf_left():
-  lab = fresh_lab("left")
-  return flatten([
-    read_reg_to("mem_ptr", lab+"+1"),
-    lab_def(lab), 
-    add(DUMMY, -(len(register(""))-1), lab+"+5"),
-    write_reg(DUMMY, "mem_ptr")
-  ])
+    lab = fresh_lab("left")
+    return flatten([
+        read_reg_to("mem_ptr", lab+"+1"),
+        lab_def(lab),
+        add(DUMMY, -(len(register(""))-1), lab+"+5"),
+        write_reg(DUMMY, "mem_ptr")
+    ])
+
 
 def bf_right():
-  lab = fresh_lab("right")
-  return flatten([
-    read_reg_to("mem_ptr", lab+"+1"),
-    lab_def(lab), 
-    add(DUMMY, len(register(""))-1, lab+"+5"),
-    write_reg(DUMMY, "mem_ptr"),
-    write_mem_cell()
-  ])
+    lab = fresh_lab("right")
+    return flatten([
+        read_reg_to("mem_ptr", lab+"+1"),
+        lab_def(lab),
+        add(DUMMY, len(register(""))-1, lab+"+5"),
+        write_reg(DUMMY, "mem_ptr"),
+        write_mem_cell()
+    ])
+
 
 def bf_inp():
-  lab = fresh_lab("inp")
-  return flatten([
-    inp(lab+"+1"),
-    lab_def(lab),
-    write_reg(DUMMY, "mem_val"),
-    write_mem()
-  ])
+    lab = fresh_lab("inp")
+    return flatten([
+        inp(lab+"+1"),
+        lab_def(lab),
+        write_reg(DUMMY, "mem_val"),
+        write_mem()
+    ])
+
 
 def bf_out():
-  lab = fresh_lab("out")
-  return flatten([
-    read_mem(),
-    read_reg_to("mem_val", lab+"+1"),
-    lab_def(lab),
-    out(DUMMY)
-  ])
+    lab = fresh_lab("out")
+    return flatten([
+        read_mem(),
+        read_reg_to("mem_val", lab+"+1"),
+        lab_def(lab),
+        out(DUMMY)
+    ])
+
 
 def bf_loop_start(loop_lab):
-  lab = fresh_lab("loop")
-  return flatten([
-    lab_def(loop_lab+"_start"),
-    read_mem(),
-    read_reg_to("mem_val", lab+"+1"),
-    lab_def(lab),
-    jz(DUMMY, loop_lab+"_end")
-  ])
+    lab = fresh_lab("loop")
+    return flatten([
+        lab_def(loop_lab+"_start"),
+        read_mem(),
+        read_reg_to("mem_val", lab+"+1"),
+        lab_def(lab),
+        jz(DUMMY, loop_lab+"_end")
+    ])
+
 
 def bf_loop_end(loop_lab):
-  return flatten([
-    jz(0, loop_lab+"_start"),
-    lab_def(loop_lab+"_end")
-  ])
+    return flatten([
+        jz(0, loop_lab+"_start"),
+        lab_def(loop_lab+"_end")
+    ])
+
 
 def compile(prog):
-  code = flatten([
-    jz(0, "defns"),
-    register("mem_ptr", "mem_start"),
-    register("mem_val"),
-    write_mem_cell(True),
-    write_mem(True),
-    read_mem(True),
-    lab_def("defns"),
-    write_mem_cell()
-  ])
+    code = flatten([
+        jz(0, "defns"),
+        register("mem_ptr", "mem_start"),
+        register("mem_val"),
+        write_mem_cell(True),
+        write_mem(True),
+        read_mem(True),
+        lab_def("defns"),
+        write_mem_cell()
+    ])
 
-  loop_stack = []
-  for instr in list(prog):
-    if instr == "+":
-      code += bf_incr()
-    elif instr == "-":
-      code += bf_decr()
-    elif instr == "<":
-      code += bf_left()
-    elif instr == ">":
-      code += bf_right()
-    elif instr == ",":
-      code += bf_inp()
-    elif instr == ".":
-      code += bf_out()
-    elif instr == "[":
-      lab = fresh_lab("loop_body")
-      loop_stack.append(lab)
-      code += bf_loop_start(lab)
-    elif instr == "]":
-      lab = loop_stack[-1]
-      loop_stack = loop_stack[:-1]
-      code += bf_loop_end(lab)
+    loop_stack = []
+    for instr in list(prog):
+        if instr == "+":
+            code += bf_incr()
+        elif instr == "-":
+            code += bf_decr()
+        elif instr == "<":
+            code += bf_left()
+        elif instr == ">":
+            code += bf_right()
+        elif instr == ",":
+            code += bf_inp()
+        elif instr == ".":
+            code += bf_out()
+        elif instr == "[":
+            lab = fresh_lab("loop_body")
+            loop_stack.append(lab)
+            code += bf_loop_start(lab)
+        elif instr == "]":
+            lab = loop_stack[-1]
+            loop_stack = loop_stack[:-1]
+            code += bf_loop_end(lab)
 
-  code += [99, "mem_start:"]
+    code += [99, "mem_start:"]
 
-  return remove_labels(code)
+    return remove_labels(code)
 
 
 prog = ",>,<[->+<]>."
@@ -282,15 +319,5 @@ iprog = compile(prog)
 
 print(iprog)
 
-from intcode import Machine
 
 print(Machine(iprog, [5, 6]).run())
-
-
-
-
-
-
-
-
-
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -1,185 +1,273 @@
 from collections import defaultdict
 
-def load_program(file): 
-  return list(map(int, open(file).read().split(",")))
+"""
+Intcode virtual machine
 
-op_lens = {1:4, 2:4, 3:2, 4:2, 5:3, 6:3, 7:4, 8:4, 9:2}
+Breif Intcode spec, see aoc 2019 for further details:
+
+A program is a list of integers.
+There are 10 opcodes:
+1 b b a: addition
+2 b b a: multiplication
+3 a: input
+4 b: output
+5 b b: jump (to second arg) if nonzero
+6 l b: jump (to second arg) if zero
+7 b b a: less than
+8 b b a: equal to
+9 b: adjust the relative base
+99: halt
+
+a = output operand (output is written to this adress); b = input operand.
+There are 3 addressing modes, determined by the upper digits of the opcode:
+0: direct
+1: immediate (not used for writes)
+2: relative indirect
+"""
+
+
+def load_program(file):
+    """
+    Loads an intcode program from a file.
+    """
+    return list(map(int, open(file).read().split(",")))
+
+
+# The lengths of each opcode.
+op_lens = {1: 4, 2: 4, 3: 2, 4: 2, 5: 3, 6: 3, 7: 4, 8: 4, 9: 2}
+
 
 def default_inpfun(self):
-  if len(self.inp) <= self.inp_ptr:
-    return "wait"
-  else:
-    # don't input.pop(0) because day 7 requires knowing the last output 
-    val = self.inp[self.inp_ptr]
-    self.inp_ptr += 1
-    return val
+    """
+    The default input function. 
+    It will simply read from its input buffer and wait when it's empty.
+    """
+    if len(self.inp) <= self.inp_ptr:
+        return "wait"
+    else:
+        # don't input.pop(0) because day 7 requires knowing the last output
+        val = self.inp[self.inp_ptr]
+        self.inp_ptr += 1
+        return val
+
 
 def default_outfun(self, val):
-  self.out.append(val)
+    """
+    The default output function.
+    It will simply write to its output buffer
+    """
+    self.out.append(val)
+
 
 def ascii_input(self):
-  if len(self.inp) == 0:
-    self.inp = [ord(c) for c in input()+'\n']
-  val = self.inp.pop(0)
-  return val
+    """
+    An input function that prompts the user for an ascii string.
+    """
+    if len(self.inp) == 0:
+        self.inp = [ord(c) for c in input()+'\n']
+    val = self.inp.pop(0)
+    return val
+
 
 def ascii_output(self, val):
-  if type(self.out) != list:
-    self.out = []
-  self.out.append(val)
-  if val < 256:
-    print(chr(val), end="")
+    """
+    An output function that prints the output to the screen as ascii.
+    """
+    if type(self.out) != list:
+        self.out = []
+    self.out.append(val)
+    if val < 256:
+        print(chr(val), end="")
+
 
 class Machine:
- 
-  def __init__(self, prog, inp=None, out=None, name=None):
-    if type(prog) == str:
-      prog = load_program(prog)
-    self.inplen = len(prog)
-    self.prog = defaultdict(int)
-    for i in range(0, len(prog)):
-      self.prog[i] = prog[i]
-    self.inp = inp if type(inp) == list else [ord(c) for c in inp] if type(inp) == str else [] 
-    self.inpfun = default_inpfun if type(inp) == list or type(inp) == str or inp == None else inp
-    self.out = out if type(out) == list else []
-    self.outfun = default_outfun if out == None or type(out) == list else out
-    self.name = name if name != None else ""
-    self.pc = 0
-    self.inp_ptr = 0
-    self.waiting = False
-    self.halted = False
-    self.rel_base = 0
+    """
+    An intcode machine. It has a running program, and input/output buffers.
 
-  def step(self):
-    if self.halted: 
-      return
+    The fields self.inp, self.out, and self.np_ptr determine the state of the I/O buffers while the machine is running.
 
+    Arguments:
+    - prog: The progrm, which is copied internally. (so it's safe to use one program to initialise multiple machines).
+        If it's a string, it's treated as a filename to load the program from.
+    - inp: If it's a list or string, set the input buffer accordingly. 
+        The internal buffer will alias inp if it's a list.
+        inp may also be a function that will be called when the machine needs to input.
+        It should return an integer, character, or the string "wait". 
+        When "wait" is returned, the self.waiting field of the machine is set.
+    - out: If it's a list, the internal output buffer will alias it.
+        If it's a function, it's called whenever the machine outputs. 
+        It is responsible for appending to the output buffer if desired.
+    - name: The name of the machine, printed in debug logs and stored in the name field.
+    """
 
-
-    prog = self.prog
-    pc = self.pc
-    
-    #print(str(pc) + ", ", end = "")
-
-    instr = prog[pc]
-    op = instr%100
-
-    if op == 1:
-      self.arith_op(lambda x, y: x+y)
-    elif op == 2:
-      self.arith_op(lambda x, y: x*y)
-    elif op == 3:
-      val = self.inpfun(self)
-      if val == "wait":
-        self.waiting = True
-        return
-      else:
-        if type(val) == str:
-          val = ord(val)
-        prog[self.indr_write(1)] = val
+    def __init__(self, prog, inp=None, out=None, name=None):
+        if type(prog) == str:
+            prog = load_program(prog)
+        self.inplen = len(prog)
+        self.prog = defaultdict(int)
+        for i in range(0, len(prog)):
+            self.prog[i] = prog[i]
+        self.inp = inp if type(inp) == list else [ord(
+            c) for c in inp] if type(inp) == str else []
+        self.inpfun = default_inpfun if type(inp) == list or type(
+            inp) == str or inp == None else inp
+        self.out = out if type(out) == list else []
+        self.outfun = default_outfun if out == None or type(
+            out) == list else out
+        self.name = name if name != None else ""
+        self.pc = 0
+        self.inp_ptr = 0
         self.waiting = False
-    elif op == 4:
-      self.outfun(self, self.indr(1))
-      
-    elif op == 5:
-      if self.indr(1) != 0:
-        self.pc = self.indr(2) 
-        return
-    elif op == 6:
-      if self.indr(1) == 0:
-        self.pc = self.indr(2) 
-        return
-    elif op == 7:
-      self.arith_op(lambda x, y: int(x<y))
-    elif op == 8:
-      self.arith_op(lambda x, y: int(x==y))
-    elif op == 9:
-      self.rel_base += self.indr(1)
-    elif op == 99:
-      self.halted = True
-      return
+        self.halted = False
+        self.rel_base = 0
 
-    else:
-      self.debug()
-      raise Exception("bad opcode " + str(op) + " at " + str(pc))
+    def step(self):
+        """
+        Runs the machine for one step.
+        The fields waiting and halted are set to reflect the state of the machine.
+        """
+        if self.halted:
+            return
 
-    self.pc = pc + op_lens[op]  
+        prog = self.prog
+        pc = self.pc
 
-  def run(self, debugging=False, ascii=False):
-    while not self.halted:
-      self.step()
-      if debugging:
-        self.debug()
-      if self.waiting and self.inpfun == default_inpfun:
-        self.debug()
-        raise "Ran out of input!"
-    return self.out if not ascii else ''.join([chr(c) for c in self.out if c < 256])
+        # print(str(pc) + ", ", end = "")
 
-  def run_until_input(self, ascii=False):
-    out_ptr = len(self.out)
-    self.step()
-    while not (self.halted or self.waiting):
-      #print(self.pc)
-      self.step()
-    ret = self.out[out_ptr:]
-    return ret if not ascii else ''.join([chr(c) for c in ret if c < 256])
+        instr = prog[pc]
+        op = instr % 100
 
-  def send_input(self, inp):
-    if type(inp) == list:
-      self.inp += inp
-    elif type(inp) == str:
-      self.inp += [ord(c) for c in inp]
-    elif type(inp) == int:
-      self.inp.append(inp)
-    else:
-      raise "Unexpected input type!"
+        if op == 1:
+            self.arith_op(lambda x, y: x+y)
+        elif op == 2:
+            self.arith_op(lambda x, y: x*y)
+        elif op == 3:
+            val = self.inpfun(self)
+            if val == "wait":
+                self.waiting = True
+                return
+            else:
+                if type(val) == str:
+                    val = ord(val)
+                prog[self.addr(1)] = val
+                self.waiting = False
+        elif op == 4:
+            self.outfun(self, self.read(1))
 
-  def indr(self, off):
-    prog, pos = self.prog, self.pc
-    mode = param(prog[pos], off)
-    if mode == 1:
-      return prog[pos+off]
-    elif mode == 2:
-      return prog[prog[pos+off]+self.rel_base]
-    else:
-      return prog[prog[pos+off]]
+        elif op == 5:
+            if self.read(1) != 0:
+                self.pc = self.read(2)
+                return
+        elif op == 6:
+            if self.read(1) == 0:
+                self.pc = self.read(2)
+                return
+        elif op == 7:
+            self.arith_op(lambda x, y: int(x < y))
+        elif op == 8:
+            self.arith_op(lambda x, y: int(x == y))
+        elif op == 9:
+            self.rel_base += self.read(1)
+        elif op == 99:
+            self.halted = True
+            return
 
-  def indr_write(self, off):
-    prog, pos = self.prog, self.pc
-    mode = param(prog[pos], off)
-    if mode == 2:
-      return prog[pos+off]+self.rel_base
-    else:
-      return prog[pos+off]
+        else:
+            self.debug()
+            raise Exception("bad opcode " + str(op) + " at " + str(pc))
 
-  def arith_op(self, f):
-    prog, pc = self.prog, self.pc
-    prog[self.indr_write(3)] = f(self.indr(1), self.indr(2))
+        self.pc = pc + op_lens[op]
 
-  def debug(self):
-    (prog, pc) = (self.prog, self.pc)
-    if self.name != "":
-      print("Machine name: " + self.name)
-    print("Program Counter: " + str(self.pc))
-    print("Current Opcode: " + str(prog[pc]))
-    print("Rel Base: " + str(self.rel_base))
+    def run(self, debugging=False, ascii=False):
+        """
+        Runs the machine to completion. Raises an error if it gets stuck waiting for input.
 
-    i = 0
-    while i < self.inplen or i < self.pc+6:
-      op = prog[i]%100
-      leng = op_lens[op] if op in op_lens else 1
-      print("[" + str(i) + "] ", end = "")
-      for j in range(0, leng):
-        print(str(prog[i+j]) + ", ", end="")
-      print(" <=========== " if i == pc else "")
-      i+=leng
-    print(prog[i])
-    
-    print()
-    print()
+        Returns the contents of the output buffer. If the ascii flag is set, it's returned as a string.
 
-def param(instr, off): 
-  return instr//(10**(off+1)) %10
+        If the debugging flag is set, print debugging information.
+        """
+        while not self.halted:
+            self.step()
+            if debugging:
+                self.debug()
+            if self.waiting and self.inpfun == default_inpfun:
+                self.debug()
+                raise Exception("Ran out of input!")
+        return self.out if not ascii else ''.join([chr(c) for c in self.out if c < 256])
+
+    def run_until_input(self, ascii=False):
+        """
+        Runs the machine to completion or until it gets stuck waiting for input.
+
+        Returns the contents of the output buffer. If the ascii flag is set, it's returned as a string.
+
+        If the debugging flag is set, print debugging information.
+        """
+        out_ptr = len(self.out)
+        self.step()
+        while not (self.halted or self.waiting):
+            # print(self.pc)
+            self.step()
+        ret = self.out[out_ptr:]
+        return ret if not ascii else ''.join([chr(c) for c in ret if c < 256])
+
+    def send_input(self, inp):
+        """
+        Appends inp to the input buffer. inp can be an int, a list of ints, or a string.
+        """
+        if type(inp) == list:
+            self.inp += inp
+        elif type(inp) == str:
+            self.inp += [ord(c) for c in inp]
+        elif type(inp) == int:
+            self.inp.append(inp)
+        else:
+            raise "Unexpected input type!"
+
+    def read(self, off):
+        """Reads the off'th operand of the current opcode, taking the adressing mode into account."""
+        return self.prog[self.addr(off)]
+
+    def addr(self, off):
+        """The adress to read from or write to to handle the off'th operand of the current opcode"""
+        prog, pos = self.prog, self.pc
+        mode = getmode(prog[pos], off)
+        if mode == 1:
+            return pos+off
+        if mode == 2:
+            return prog[pos+off]+self.rel_base
+        else:
+            return prog[pos+off]
+
+    def arith_op(self, f):
+        """Common implementation of the binary arithmatic opcodes"""
+        prog, _ = self.prog, self.pc
+        prog[self.addr(3)] = f(self.read(1), self.read(2))
+
+    def debug(self):
+        """ Prints debug info """
+        (prog, pc) = (self.prog, self.pc)
+        if self.name != "":
+            print("Machine name: " + self.name)
+        print("Program Counter: " + str(self.pc))
+        print("Current Opcode: " + str(prog[pc]))
+        print("Rel Base: " + str(self.rel_base))
+
+        i = 0
+        while i < self.inplen or i < self.pc+6:
+            op = prog[i] % 100
+            leng = op_lens[op] if op in op_lens else 1
+            print("[" + str(i) + "] ", end="")
+            for j in range(0, leng):
+                print(str(prog[i+j]) + ", ", end="")
+            print(" <=========== " if i == pc else "")
+            i += leng
+        print(prog[i])
+
+        print()
+        print()
 
 
-
+def getmode(instr, off):
+    """Coputes the adressing mode for the off'th parameter of instr"""
+    return instr//(10**(off+1)) % 10

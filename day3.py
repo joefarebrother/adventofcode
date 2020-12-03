49eb5bd2220cacd2ca10442fafa000b3efa984b6
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-03 06:35:00 +0000

    Use complex numbers as positions

--- day3.py
+++ day3.py
@@ -7,23 +7,25 @@ grid = readlines("input3")
 trees = 0
 
 
-def trees_for_slope(xinc, yinc):
+def check_tree(pos: complex):
+    return grid[int(pos.imag)][int(pos.real) % (len(grid[0]))] == '#'
+
+
+def trees_for_slope(slope):
     trees = 0
 
-    xpos, ypos = 0, 0
-    while ypos < len(grid):
-        if grid[ypos][xpos] == '#':
+    pos = 0j
+    while pos.imag < len(grid):
+        if check_tree(pos):
             trees += 1
 
-        ypos += yinc
-        xpos += xinc
-        xpos %= len(grid[0])
+        pos += slope
 
     return trees
 
 
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-treecounts = mapl(lambda xy: trees_for_slope(xy[0], xy[1]), slopes)
+slopes = [1+1j, 3+1j, 5+1j, 7+1j, 1+2j]
+treecounts = mapl(trees_for_slope, slopes)
 
 print(treecounts)
 print(math.prod(treecounts))

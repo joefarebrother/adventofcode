df579c48a06542d55a88d00ae5bce750dfd9c365
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-04 14:47:32 +0000

    Rewrite day 2 using regex

--- day2.py
+++ day2.py
@@ -1,32 +1,29 @@
 # pylint: disable=unused-wildcard-import
 from utils import *
 
-data = list(open("input2"))
-
-data = mapl(lambda l: l.split(":"), data)
+data = readlines("input2")
 
 
-def parse_pol(pol):
-    (lo, hi) = ints_in(pol)
-    char = pol[-1]
-    return (lo, hi, char)
+def parse(line):
+    (lo, hi, ch, pw) = re.fullmatch(r'(\d+)-(\d+) (.):(.*)', line).groups()
+    return (int(lo), int(hi), ch, pw)
 
 
-def valid(pol, pw):
-    (lo, hi, ch) = parse_pol(pol)
+def valid(line):
+    (lo, hi, ch, pw) = parse(line)
     cnt = list(pw).count(ch)
     return lo <= cnt <= hi
 
 
-def valid2(pol, pw):
-    (lo, hi, ch) = parse_pol(pol)
+def valid2(line):
+    (lo, hi, ch, pw) = parse(line)
     # since we don't strip the leading space in pw, we get 1-based indexing for free :)
     return (pw[lo] == ch) ^ (pw[hi] == ch)
 
 
 validcount = 0
-for (pol, pw) in data:
-    if valid2(pol, pw):
+for line in data:
+    if valid2(line):
         validcount += 1
 
 print(validcount)

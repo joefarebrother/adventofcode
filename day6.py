225036dc106bd6c117044c865b6adfdd49dc8f18
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-06 05:24:19 +0000

    Day 6

---
+++ day6.py
@@ -0,0 +1,22 @@
+# pylint: disable=unused-wildcard-import
+from utils import *
+
+data = [grp.strip() for grp in open("input6").read().split("\n\n")]
+
+# ans = 0
+# for i in data:
+#     uniq = set(filter(lambda c: c.islower(), list(i)))
+#     print(uniq, len(uniq))
+#     ans += len(uniq)
+#     print(ans)
+
+print(data)
+
+ans = 0
+for grp in data:
+    all = set(list("qwertyuiopasdfghjklzxcvbnm"))
+    for per in grp.split("\n"):
+        qs = set(filter(lambda c: c.islower(), list(per)))
+        all &= qs
+    ans += len(all)
+    print(ans, all)

97e641abce006736ba181a2f1575de1b995522e3
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-22 06:30:42 +0000

    Day 22

--- day22.py
+++ day22.py
@@ -32,7 +32,6 @@ p1 = ints(p1.split("\n")[1:])
 p2 = ints(p2.split("\n")[1:])
 
 
-@cache
 def play(p1, p2):
     p1, p2 = list(p1), list(p2)
     states = set()
@@ -47,16 +46,16 @@ def play(p1, p2):
         c1 = p1.pop(0)
         c2 = p2.pop(0)
         if c1 <= len(p1) and c2 <= len(p2):
-            (w, _) = play(tuple(p1), tuple(p2))
+            (w, _) = play(p1[:c1], p2[:c2])
         else:
             w = 1 if c1 > c2 else 2
         w, wc, lc = [0, (p1, c1, c2), (p2, c2, c1)][w]
         w.append(wc)
         w.append(lc)
     if p1:
-        return 1, p1
+        return (1, p1)
     else:
-        return 2, p2
+        return (2, p2)
 
 
 (_, w) = play(tuple(p1), tuple(p2))

27e8a3ca5bfad8d4a873e14e4e1c99de617f4ee9
Joe Farebrother <joseph.farebrother@gmail.com>
2020-12-25 05:29:48 +0000

    Day 25

---
+++ day25.py
@@ -0,0 +1,27 @@
+# pylint: disable=unused-wildcard-import
+from utils import *
+
+keys = ints(readlines("25.in"))
+#keys = [5764801, 17807724]
+
+# transform(sub): subject ^ loop_size % 20201227
+# keys = transform(7, loop_1), transform(7, loop_2)
+# answer = transform(key_1, loop_2) = transform(key+2, loop_1) = 7 ^ (loop_1*loop_2)
+
+MOD = 20201227
+
+
+def find_loop(key):
+    i = 0
+    tr = 1
+    while True:
+        i += 1
+        tr *= 7
+        tr %= MOD
+        if tr == key:
+            return i
+
+
+loops = mapl(find_loop, keys)
+print(loops)
+print(pow(7, math.prod(loops), MOD))

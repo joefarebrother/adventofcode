be48551b9ea74e9858a2131dd7312af835863a33
Joe Farebrother <joseph.farebrother@gmail.com>
2021-12-02 22:51:50 +0000

    Day 2 cleaned up solution

---
+++ day2.py
@@ -0,0 +1,20 @@
+from utils import *
+
+inp = readlines(2)
+
+x = 0
+d = 0
+aim = 0
+for line in inp:
+    cmd, amt = line.split(" ")
+    amt = int(amt)
+    if cmd == "forward":
+        x += amt
+        d += amt*aim
+    elif cmd == "down":
+        aim += amt
+    elif cmd == "up":
+        aim -= amt
+
+print("Part 1: ", x*aim)
+print("Part 2: ", x*d)

a50a303f19f32e374b9c1b03aa189644e4e0c6f3
Joe Farebrother <joseph.farebrother@gmail.com>
2021-12-02 06:29:35 +0000

    Day 2

--- input_utils.py
+++ input_utils.py
@@ -1,5 +1,6 @@
 import os
-from datetime import date
+from datetime import datetime
+from time import sleep
 
 from misc_utils import ints_in
 
@@ -48,7 +49,9 @@ def filename_for(f):
 
 def get_input(day=None, year=2021, filename=None, verbose=True):
     """
-    Downloads and returns the input, as a list of lines. Defaults to today. If verbose is true, prints some stats.
+    Downloads and returns the input, as a list of lines; or a list of ints if every line is an int. 
+    Defaults to today. 
+    If verbose is true, prints some stats.
     """
     def printv(s):
         if verbose:
@@ -64,11 +67,21 @@ def get_input(day=None, year=2021, filename=None, verbose=True):
     def csv_numeric(s):
         all(numeric(x) for x in s.split(","))
 
+    now = datetime.now()
+
     if day == None:
-        day = date.today().day
+        day = now.day
+
+    waited = False
+
+    if (now.day, now.month, now.year) == (day, 12, year) and now.hour < 5:
+        unlock = now.replace(hour=5, minute=0, second=1)
+        diff = (unlock-now).total_seconds()
+        printv(f"Waiting {diff} seconds")
+        sleep(diff)
 
     filename = filename_for(day)
-    if not os.path.isfile(filename) or open(filename).read().strip() == "":
+    if not os.path.isfile(filename) or open(filename).read().strip() == "" or waited:
         printv(f"Downloading input for {year} day {day} to {filename}")
         os.system(f"bash -c './aocfetch {day} {year} > {filename}'")
     else:
@@ -79,8 +92,8 @@ def get_input(day=None, year=2021, filename=None, verbose=True):
     if verbose:
         print(f"Lines: {len(inp)}")
         print(f"Blank lines: {inp.count('')}")
-        print(f"Numeric lines: {len([numeric(l) for l in inp])}")
-        print(f"CSV Numeric lines: {len([csv_numeric(l) for l in inp])}")
+        print(f"Numeric lines: {sum(numeric(l) for l in inp)}")
+        print(f"CSV Numeric lines: {sum(csv_numeric(l) for l in inp)}")
         if len(set(len(l) for l in inp)) == 1 and len(inp) != 1:
             print("Looks like a grid")
 
@@ -92,5 +105,8 @@ def get_input(day=None, year=2021, filename=None, verbose=True):
         if len(inp) < 10 and all(len(l) < 100 for l in inp):
             for l in inp:
                 print(l)
+
+    if all(numeric(l) for l in inp):
+        return ints(inp)
 
     return inp

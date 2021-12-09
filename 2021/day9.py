a4b8ab37c0460e68e0415c8bc1fa1cf7895558da
Joe Farebrother <joseph.farebrother@gmail.com>
2021-12-09 06:06:48 +0000

    Rewrite part 2 in terms of BFS

--- 2021/day9.py
+++ 2021/day9.py
@@ -1,41 +1,27 @@
 from utils import *
 
-inp = Grid(9)
+inp = Grid(9, default=9)
 for p in inp:
     inp[p] = int(inp[p])
 
-low = Grid(inp)
-for p in low:
-    low[p] = None
+low = set()
 
 risk = 0
 for p, v in inp.items():
     if all(dp not in inp or inp[dp] > v for dp in neighbours(p)):
-        low[p] = p
+        low.add(p)
         risk += v+1
 
 print("Part 1: ", risk)
 
 
-def prop_base():
-    global low, inp
-    changed = False
-    for p in inp:
-        if inp[p] != 9 and low[p] is None:
-            for dp in neighbours(p):
-                if dp in inp and low[dp] is not None:
-                    low[p] = low[dp]
-                    changed = True
-    return changed
+def adj(p):
+    return [dp for dp in neighbours(p) if inp[dp] != 9]
 
 
-while prop_base():
-    pass
+ct = []
+for l in low:
+    ct.append(sum(1 for _ in FGraph(adj).BFS_gen(l)))
 
-ct = defaultdict(int)
-for v in low.values():
-    if v is not None:
-        ct[v] += 1
-
-big = sorted(ct.values(), reverse=True)[:3]
+big = sorted(ct, reverse=True)[:3]
 print("Part 2: ", big, math.prod(big))

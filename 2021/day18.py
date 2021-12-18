c4392c440b3afb8665c02f85a955316e4ed3944e
Joe Farebrother <joseph.farebrother@gmail.com>
2021-12-18 06:54:43 +0000

    Day 18 cleanups

--- 2021/day18.py
+++ 2021/day18.py
@@ -4,6 +4,20 @@ oginp = readlines(18)
 inp = [eval(i) for i in oginp]
 
 
+def add_adjacent(n, pars, idx):
+    opp = 1-idx
+    for (p, i) in reversed(pars):
+        if i == opp:
+            if type(p[idx]) == int:
+                p[idx] += n[idx]
+            else:
+                p = p[idx]
+                while type(p[opp]) != int:
+                    p = p[opp]
+                p[opp] += n[idx]
+            return
+
+
 def check_explodes(n, pars=None):
     if pars == None:
         pars = []
@@ -11,44 +25,19 @@ def check_explodes(n, pars=None):
         return False
     if len(pars) < 4:
         return check_explodes(n[0], pars+[(n, 0)]) or check_explodes(n[1], pars+[(n, 1)])
-    # find left
-    #printx("Exploding:", n)
-    for (p, i) in reversed(pars):
-        if i == 1:  # first ancestor for which this is the right child of
-            if type(p[0]) == int:
-                p[0] += n[0]
-                break
-            else:
-                p = p[0]
-                while type(p[1]) != int:
-                    p = p[1]
-                p[1] += n[0]
-            break
-    # find right
-    for (p, i) in reversed(pars):
-        if i == 0:  # first ancestor for which this is the left child of
-            if type(p[1]) == int:
-                p[1] += n[1]
-                break
-            else:
-                p = p[1]
-                while type(p[0]) != int:
-                    p = p[0]
-                p[0] += n[1]
-                break
+    add_adjacent(n, pars, 0)
+    add_adjacent(n, pars, 1)
     p, i = pars[-1]
     p[i] = 0
     return True
 
 
 def check_splits(n, pars=None):
-    #printx("Checking splits:", n)
     if pars == None:
         pars = []
     if type(n) == int:
         if n < 10:
             return False
-        #printx("Splitting:", n)
         p, i = pars[-1]
         p[i] = [math.floor(n/2), math.ceil(n/2)]
         return True
@@ -62,10 +51,8 @@ def mag(n):
 
 
 def reduce(n):
-    printx("Reducing:", n)
     while check_explodes(n) or check_splits(n):
         pass
-    printx("Done reducing:", n)
     return n
 
 
@@ -80,8 +67,8 @@ for m in inp[1:]:
 print("Part 1:", mag(n))
 
 maxm = 0
-for i, j in itertools.product(oginp, repeat=2):
+for i, j in it.permutations(oginp, 2):
     m = mag(add(eval(i), eval(j)))
     maxm = max(maxm, m)
 
-print("Part 2", maxm)
+print("Part 2:", maxm)

18c1e1866d7b9e18c319aafcd764cc79bba38b4d
Joe Farebrother <joseph.farebrother@gmail.com>
2021-12-23 14:23:40 +0000

    Day 23 more efficiently; add DAG search

--- 2021/day23.py
+++ 2021/day23.py
@@ -21,54 +21,55 @@ g.draw()
 
 rooms = {l: [x+y*1j for y in [2, 3, 4, 5]]
          for x in [3, 5, 7, 9] for l in ["___A_B_C_D"[x]]}
-hallway = [x+1j for x in irange(1, g.width()-1) if x not in [3, 5, 7, 9]]
+hallway = [x+1j for x in irange(1, g.width()-2) if x not in [3, 5, 7, 9]]
 energy = {l: 10**i for i, l in enumerate("ABCD")}
 
 occupied = {p: l for p, l in g.items() if l in "ABCD"}
-printx(occupied)
 
 
 def path(occ, start, end):
-    def empty(p):
-        return g[p] != "#" and p not in occ
-    found, d = FGraph(
-        lambda p: [n for n in neighbours(p) if empty(n)]).BFS(start, end)
-    if found:
-        return d
+    ps = ({start.real+y*1j for y in irange(1, int(start.imag))} | {end.real+y*1j for y in irange(
+        1, int(end.imag))} | {x+1j for x in irange(*ints(bounds((start.real, end.real))))}) - {start}
+    if all(p not in occ for p in ps):
+        return len(ps)
 
 
 def room_free(occ, l):
     return all(p not in occ or occ[p] == l for p in rooms[l])
 
 
+def is_home(occ, p):
+    if p not in occ:
+        return False
+    l = occ[p]
+    ps = rooms[l]
+    return p in ps and all(q in occ and occ[q] == l for q in ps if q.imag > p.imag)
+
+
 def moves(occ):
     res = []
-    # hallway or room to room
-    for h, l in occ.items():
-        ps = rooms[l]
-        if room_free(occ, l) and h not in ps:
-            i = -1
-            while ps[i] in occ:
-                i -= 1
-            p = ps[i]
-            d = path(occ, h, p)
-            if d:
-                res.append((h, p, d*energy[l]))
+    # hallway to room
+    for h in hallway:
+        if h in occ:
+            l = occ[h]
+            ps = rooms[l]
+            if room_free(occ, l):
+                i = -1
+                while ps[i] in occ:
+                    i -= 1
+                p = ps[i]
+                d = path(occ, h, p)
+                if d:
+                    res.append((h, p, d*energy[l]))
     # room to hallway
     for l, ps in rooms.items():
-        if not all(p in occ and occ[p] == l for p in ps):
-            for p in ps:
-                if p in occ:
+        for p in ps:
+            if p in occ:
+                if not is_home(occ, p):
                     for h in hallway:
                         d = path(occ, p, h)
                         if d:
                             res.append((p, h, d*energy[occ[p]]))
-    # hallway to hallway # this is incorrect
-    # for h1, h2 in it.permutations(hallway, 2):
-    #     if h1 in occ and not h2 in occ and room_free(occ, occ[h1]):
-    #         d = path(occ, h1, h2)
-    #         if d:
-    #             res.append((h1, h2, d*energy[occ[h1]]))
     return res
 
 
@@ -106,16 +107,16 @@ def print_state(occ):
 
 gr = None
 
-print_state(occupied)
-for m in adj(occupied):
-    print_state(m)
+# print_state(occupied)
+# for m in adj(occupied):
+#     print_state(m)
 
 
 gr = FGraph(adj)
 
 
 def room_dist(a, b):
-    return a.imag+b.imag-2+min(abs(a.real-b.real), 2)
+    return a.imag+b.imag-2+(abs(a.real-b.real) or 2)
 
 
 def target(occ, l):
@@ -127,19 +128,36 @@ def target(occ, l):
 def h(occ):
     occ = dict(occ)
     res = 0
+    c = Counter()
     for p, l in occ.items():
-        if p in rooms[l]:
-            ps = rooms[l]
-            if all(q in occ and occ[q] == l for q in ps if q.imag > p.imag):
-                continue
-        res += energy[l] * room_dist(p, target(occ, l))
+        if is_home(occ, p):
+            continue
+        res += energy[l] * (room_dist(p, target(occ, l)) - c[l])
+        c[l] += 1
 
     return res
 
 
 end, d = gr.astar(frozenset(occupied.items()), end_cond, h)
+# end, d = gr.DAG_search(frozenset(occupied.items()), end_cond)
 print("====")
 for occ in gr.get_path(end):
     print_state(occ)
-    print(gr.dists[occ])
+    # print(gr.dists[occ])
 print(d)
+
+
+# DAG search:
+# real    0m41.489s
+# user    0m41.338s
+# sys     0m0.137s
+
+# Dijkstra:
+# real    0m41.489s
+# user    0m41.338s
+# sys     0m0.137s
+
+# A*:
+# real    0m29.334s
+# user    0m29.219s
+# sys     0m0.113s
--- graph.py
+++ graph.py
@@ -1,7 +1,9 @@
+from functools import cache
 from heapq import heappush, heappop
 from typing import Callable
 from collections import deque, defaultdict
 from itertools import chain
+import math
 
 
 class AbGraph():
@@ -157,13 +159,15 @@ class AbGraph():
         """
         return _consume(self.BFS_gen(start), end)
 
-    def astar_gen(self, start, h=lambda _: 0):
+    def astar_gen(self, start, h=lambda _: 0, incon_cb=lambda x, y: None):
         """
         A generator that traverses the graph sing the A* algorithm / dijkstra's algorithm.
 
-        h is the heiristic function, aproximating distance to the goal.
+        h is the heuristic function, aproximating distance to the goal.
         When constant, it's djikstra's.
-        When consistent (i.e. h(x) <= d(x,y) + h(y)) the distances returned are optimal.
+        Should be admisable (doesn't overapproximate total distance to the goal) in order to return optimal distances.
+        When consistent (i.e. h(x) <= d(x,y) + h(y)), it's admisable.
+        Calls incon_cb the first time it detects that the heuristic is inconsistent.
 
         While running, self.pqueue is set to the priority queue, and self.dists is set to the distances map.
         These shouldn't be modified.
@@ -175,6 +179,7 @@ class AbGraph():
         dists = {self.key(start): 0}
         self.dists = dists
         self.prev = {}
+        warned = False
         while len(pqueue) > 0:
             _, d, _, node, prev = heappop(pqueue)
             if dists[self.key(node)] < d:
@@ -187,13 +192,21 @@ class AbGraph():
                 k = self.key(next)
                 if k in dists and dists[k] <= d+nd:
                     continue
+                if not warned:
+                    hx = h(node)
+                    hy = h(next)
+                    if hx > nd + hy:
+                        warned = True
+                        print("Warning: heuristic not consistent: x=",
+                              node, " y=", next, "d=", nd, "hx-hy=", hx-hy, "hx=", hx, "hy=", hy)
+                        incon_cb(node, next)
                 dists[k] = d+nd
                 i += 1
                 heappush(pqueue, (d+nd+h(next), d+nd, i, next, node))
 
     dijkstra_gen = astar_gen
 
-    def astar(self, start, end, h=lambda _: 0):
+    def astar(self, start, end, h=lambda _: 0, incon_cb=lambda x, y: None):
         """
         Traverses the graph using the A* algorithm / djikstra's algorithm entil the end is reached or the search space is exhausted.
         end may be a predicate, a value (in which case it's compared to node), or None.
@@ -202,14 +215,54 @@ class AbGraph():
         When constant 0 (the default), it's djikstra's.
         When admisable (never overetimates distance to the goal), returned paths are optimal.
         When consistent (never overestimates a single step - i.e. h(x) <= h(y) + d(x,y)), it's admisable and the algorithm is optimally efficient.
+        Calls incon_cb the first time it detects that the heuristic is inconsistent.
 
         Returns:
         - (endpoint, d) if end was found at distance d from the start.
         - (None, dists) if end was not found, and dist is the distances map.
         """
-        return _consume(self.astar_gen(start, h), end, lambda: self.dists)
+        return _consume(self.astar_gen(start, h, incon_cb), end, lambda: self.dists)
 
     dijkstra = astar
+
+    def DAG_search(self, start, end):
+        """
+        If the graph is acyclic, finds the shortest path from start to end.
+
+        Returns:
+        - (endpoint, d) if end was found at dist d from the start
+        - (None, None) otherwise
+        """
+        self.prev = {}
+        cache = {}
+        sent = object()
+
+        def go(s):
+            if _is_end(end, s):
+                return (0, [s])
+            k = self.key(s)
+            if k in cache:
+                r = cache[k]
+                if r is sent:
+                    raise Exception("Graph has a cycle", s)
+                return r
+            cache[k] = sent
+            mind = math.inf
+            minp = None
+            for next, d in self[s].items():
+                nd, np = go(next)
+                nd += d
+                if nd < mind:
+                    mind = nd
+                    minp = [s]+np
+            cache[k] = mind, minp
+            return mind, minp
+        d, ps = go(start)
+        if d < math.inf:
+            for p, n in zip(ps, ps[1:]):
+                self.prev[n] = p
+            return ps[-1], d
+        return (None, None)
 
 
 def _consume(gen, end, onfail=None):
@@ -236,6 +289,7 @@ class FGraph(AbGraph):
     """
 
     def __init__(self, adj: Callable, key=None):
+        super().__init__()
         self._adj = adj
         self._key = key
 
@@ -254,6 +308,7 @@ class DGraph(AbGraph):
     """
 
     def __init__(self, adj: dict, key=None):
+        super().__init__()
         self._adj = adj
         self._key = key
 

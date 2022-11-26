556ab2a8e3e86ffb7df0d9cbf37ca830a0d99d36
Joe Farebrother <joefarebrother@github.com>
2022-11-26 22:03:11 +0000

    Unindex page files that were meant to be gitignored

--- reorganise.py
+++ reorganise.py
@@ -16,21 +16,24 @@ def move_if_able(src, dest):
 for year in range(2017, 2022):
     for day in range(1, 26):
         daydir = f"{curdir}/{year}/{day}"
-        os.makedirs(daydir)
-        move_if_able(f"{curdir}/{year}/day{day}.py", f"{daydir}/sol.py")
-        move_if_able(f"{curdir}/{year}/{day}.in", f"{daydir}/real.in")
-        testdir = f"{curdir}/test/{year}/{day}"
-        if os.path.isdir(testdir):
-            for f in os.listdir(testdir):
-                m = re.fullmatch(r'input(\d+)', f)
-                if m:
-                    idx = m.groups(1)[0]
-                    shutil.move(f"{testdir}/{f}", f"{daydir}/test{idx}.in")
-                else:
-                    m = re.fullmatch(r'output(\d+)-(\d)', f)
-                    if m:
-                        idx, part = m.groups(1)
-                        shutil.move(f"{testdir}/{f}",
-                                    f"{daydir}/test{idx}-part{part}.out")
-                    else:
-                        shutil.move(f"{testdir}/{f}", f"{daydir}/{f}")
+        # os.makedirs(daydir)
+        # move_if_able(f"{curdir}/{year}/day{day}.py", f"{daydir}/sol.py")
+        # move_if_able(f"{curdir}/{year}/{day}.in", f"{daydir}/real.in")
+        # testdir = f"{curdir}/test/{year}/{day}"
+        # if os.path.isdir(testdir):
+        #     for f in os.listdir(testdir):
+        #         m = re.fullmatch(r'input(\d+)', f)
+        #         if m:
+        #             idx = m.groups(1)[0]
+        #             shutil.move(f"{testdir}/{f}", f"{daydir}/test{idx}.in")
+        #         else:
+        #             m = re.fullmatch(r'output(\d+)-(\d)', f)
+        #             if m:
+        #                 idx, part = m.groups(1)
+        #                 shutil.move(f"{testdir}/{f}",
+        #                             f"{daydir}/test{idx}-part{part}.out")
+        #             else:
+        #                 shutil.move(f"{testdir}/{f}", f"{daydir}/{f}")
+        for f in os.listdir(daydir):
+            if f.startswith("page"):
+                os.system(f"git rm --cached {daydir}/{f}")

479808965a5059ff99abf7a609dcdc5b9c58d4a0
Joe Farebrother <joefarebrother@github.com>
2022-12-02 05:41:32 +0000

    Day 2 cleanup

--- 2022/2/sol.py
+++ 2022/2/sol.py
@@ -1,26 +1,31 @@
 from utils import *
 
-tot = 0
-for line in inp_readlines():
-    if line:
-        fst, snd = line.split(" ")
-        fst = " ABC".find(fst)
-        snd = " XYZ".find(snd)
-        assert (fst in [1, 2, 3] and snd in [1, 2, 3])
-        score = [3, 6, 0][(snd-fst) % 3]+snd
-        print(fst, snd, score)
-        tot += score
+inp = [line.split() for line in inp_readlines()]
+
+
+def result(fst, snd):
+    """Returns 0 for a draw, 1 for a win, 2 for a loss"""
+    assert (fst in [1, 2, 3] and snd in [1, 2, 3])
+    return (snd-fst) % 3
+
 
 tot = 0
-for line in inp_readlines():
-    if line:
-        fst, res = line.split(" ")
-        fst = " ABC".find(fst)
-        res = {"X": -1, "Y": 0, "Z": 1}[res]
-        snd = mod_inc(fst+res, 3)
-        assert (fst in [1, 2, 3] and snd in [1, 2, 3])
-        score = [3, 6, 0][res]+snd
-        print(fst, snd, score)
-        tot += score
+for fst, snd in inp:
+    fst = " ABC".find(fst)
+    snd = " XYZ".find(snd)
+    score = [3, 6, 0][result(fst, snd)]+snd
+    tot += score
 
-print(tot)
+print("Part 1:", tot)
+
+tot = 0
+for fst, res in inp:
+    fst = " ABC".find(fst)
+    res = "YXZ".find(res)
+    snd = mod_inc(fst+res, 3)
+    assert res == result(fst, snd)
+    score = [3, 6, 0][res]+snd
+
+    tot += score
+
+print("Part 2:", tot)

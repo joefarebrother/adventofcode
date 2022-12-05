11bd14bfb0ed51c5128f33b91325be440298661d
Joe Farebrother <joefarebrother@github.com>
2022-12-05 05:37:03 +0000

    Day 5 cleanup

--- 2022/5/sol.py
+++ 2022/5/sol.py
@@ -1,22 +1,29 @@
 from utils import *
+from copy import deepcopy
 
 crates_raw, instrs = inp_groups()
 
-crates = [[] for _ in range(max(ints_in(crates_raw[-1]))+1)]
+crates = defaultdict(list)
 for line in crates_raw[::-1][1:]:
     row = line[1::4]
     for i, ch in enumerate(row):
         if ch != " ":
             crates[i+1].append(ch)
 
-for instr in instrs:
-    amt, src, dest = ints_in(instr)
-    #print(amt, src, dest, crates)
-    # for _ in range(amt):
-    #    crates[dest].append(crates[src].pop())
-    new, moved = crates[src][:-amt], crates[src][-amt:]
-    print(amt, src, dest, crates[src], moved, new)
-    crates[src] = new
-    crates[dest] += moved
 
-print("Part 2:", "".join(crate[-1] for crate in crates[1:]))
+def do_part(crates, part):
+    crates = deepcopy(crates)
+    for instr in instrs:
+        amt, src, dest = ints_in(instr)
+        if part == 1:
+            for _ in range(amt):
+                crates[dest].append(crates[src].pop())
+        else:
+            new, moved = crates[src][:-amt], crates[src][-amt:]
+            crates[src] = new
+            crates[dest] += moved
+    return "".join(crates[i][-1] for i in irange(len(crates)))
+
+
+print("Part 1:", do_part(crates, 1))
+print("Part 2:", do_part(crates, 2))

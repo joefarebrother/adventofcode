9a1234e19707460a457a4a21f34b7b825a38ad95
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-08 16:57:07 +0000

    Day 8 cleanup

--- 2022/8/sol.py
+++ 2022/8/sol.py
@@ -3,34 +3,30 @@ from utils import *
 inp = Grid(0, ints=True)
 
 
-def vis(p):
-    for d in neighbours(0):
-        p0 = p
-        while p0 in inp and p0+d in inp:
-            if inp[p0+d] >= inp[p]:
-                break
-            p0 += d
-        else:
-            return True
-    return False
-
-
-print(sum(vis(p) for p in inp))
-
-
-def score(p, d):
+def vis_in_dir(p, d):
+    """
+    Returns n, vis where n is the number of trees visible in direction d from point p, 
+    and vis is True if p is visible from the edge.
+    """
     p0 = p
-    i = 1
-    while p0 in inp and p0+d in inp:
-        if inp[p0+d] >= inp[p]:
-            return i
-        p0 += d
+    i = 0
+    while p in inp and p+d in inp:
+        p += d
         i += 1
-    return i-1
+        if inp[p] >= inp[p0]:
+            return i, False
+    return i, True
 
 
-def tscore(p):
-    return math.prod(score(p, d) for d in neighbours(0))
+def vis(p):
+    return any(vis_in_dir(p, d)[1] for d in neighbours(0))
 
 
-print(max(tscore(p) for p in inp))
+print("Part 1:", sum(vis(p) for p in inp))
+
+
+def score(p):
+    return math.prod(vis_in_dir(p, d)[0] for d in neighbours(0))
+
+
+print("Part 2:", max(score(p) for p in inp))

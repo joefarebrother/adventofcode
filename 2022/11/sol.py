b3f3d77f46395ff7c36622f4e33c18316b05d3bc
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-11 05:50:05 +0000

    Day 11 cleanup

--- 2022/11/sol.py
+++ 2022/11/sol.py
@@ -1,32 +1,28 @@
 from utils import *
 
-new = 0
-old = 0
+monkeys = []
+big_mod = 0
 
 
 class Monkey:
     def __init__(self, gr):
-        id, items, op, test, true, false = gr
-        self.id = ints_in(id)[0]
+        id_, items, op, test, true, false = gr
+        self.id = ints_in(id_)[0]
         self.items = ints_in(items)
-        self.op = op[len("  Operation: new ="):].strip()
+        self.op = eval("lambda old: " + op.split("=")[1].strip())
         self.test = ints_in(test)[0]
         self.true = ints_in(true)[0]
         self.false = ints_in(false)[0]
         self.activity = 0
 
-    def turn(self):
-        global old
-        if not self.items:
-            return
-
+    def turn(self, part2):
         for item in self.items:
-            old = item
-            item = eval(self.op)
+            item = self.op(item)
             # print(self.id, old, item, item//3, (item//3) % self.test, self.true, self.false)
-            # item //= 3
+            if not part2:
+                item //= 3
             item %= big_mod
-            if not (item % self.test):
+            if item % self.test == 0:
                 monkeys[self.true].items.append(item)
             else:
                 monkeys[self.false].items.append(item)
@@ -34,13 +30,17 @@ class Monkey:
         self.items = []
 
 
-monkeys = [Monkey(gr) for gr in inp_groups()]
-big_mod = math.prod((m.test for m in monkeys))
+def do(rounds, part2):
+    global monkeys, big_mod
+    monkeys = [Monkey(gr) for gr in inp_groups()]
+    big_mod = math.prod(m.test for m in monkeys)
 
-for i in range(10000):
-    for m in monkeys:
-        m.turn()
-    print(i, [m.items for m in monkeys])
+    for _ in range(rounds):
+        for m in monkeys:
+            m.turn(part2)
+    acts = sorted(m.activity for m in monkeys)
+    return math.prod(acts[-2:])
 
-acts = sorted(m.activity for m in monkeys)
-print(acts, acts[-2:], math.prod(acts[-2:]))
+
+print("Part 1:", do(20, False))
+print("Part 2:", do(10000, True))

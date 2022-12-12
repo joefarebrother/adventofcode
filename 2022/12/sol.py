410979fa84c4ada99c94c9caa43ee123823156d9
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-12 06:12:19 +0000

    Day 12 unify both parts

--- 2022/12/sol.py
+++ 2022/12/sol.py
@@ -14,31 +14,24 @@ def height(c):
     return ord(c)-ord("a")
 
 
-def adj(p):
-    for p1 in neighbours(p):
-        if p1 in inp:
-            if height(inp[p]) - height(inp[p1]) >= -1:
-                yield p1
-
-
-gr = FGraph(adj)
-dist = gr.BFS(start).dist(end)
-# path = gr.get_path(end)
-# for p0, p1 in windows(path, 2):
-#     print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))
-
-print("Part 1:", dist)
-
-
-def adji(p1):
+def adj(p1):
+    """Inverse graph handles both parts"""
     for p in neighbours(p1):
         if p in inp:
             if height(inp[p]) - height(inp[p1]) >= -1:
                 yield p
 
 
-gri = FGraph(adji)
-st, best = gri.BFS(end).find(lambda s: inp[s] == "a")
+gr = FGraph(adj)
+dist = gr.BFS(end).dist(start)
+# path = gr.get_rev_path(end)
+# for p0, p1 in windows(path, 2):
+#     print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))
+
+print("Part 1:", dist)
+
+
+st, best = gr.BFS(end).find(lambda s: inp[s] == "a")
 # path = gri.get_rev_path(st)
 # for p0, p1 in windows(path, 2):
 #     print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))

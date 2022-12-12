b78b434acf9f318c220eb0f3adf6277bf536c0b0
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-12 05:59:43 +0000

    Day 12 improve efficiency

--- 2022/12/sol.py
+++ 2022/12/sol.py
@@ -15,24 +15,32 @@ def height(c):
 
 
 def adj(p):
-    if p in inp:
-        for p1 in neighbours(p):
-            if p1 in inp:
-                if height(inp[p]) - height(inp[p1]) >= -1:
-                    yield p1
+    for p1 in neighbours(p):
+        if p1 in inp:
+            if height(inp[p]) - height(inp[p1]) >= -1:
+                yield p1
 
 
 gr = FGraph(adj)
 dist = gr.BFS(start).dist(end)
-path = gr.get_path(end)
-for p0, p1 in windows(path, 2):
-    print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))
+# path = gr.get_path(end)
+# for p0, p1 in windows(path, 2):
+#     print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))
 
 print("Part 1:", dist)
 
-best = dist
-for p in inp:
-    if inp[p] == "a":
-        best = min(best, gr.BFS(p).dist(end))
+
+def adji(p1):
+    for p in neighbours(p1):
+        if p in inp:
+            if height(inp[p]) - height(inp[p1]) >= -1:
+                yield p
+
+
+gri = FGraph(adji)
+st, best = gri.BFS(end).find(lambda s: inp[s] == "a")
+# path = gri.get_rev_path(st)
+# for p0, p1 in windows(path, 2):
+#     print(p0, p1, inp[p0], inp[p1], height(inp[p0])-height(inp[p1]))
 
 print("Part 2:", best)

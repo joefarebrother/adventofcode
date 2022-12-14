b3bab515f452eb32d67484f8aa3d984c86bff91b
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-14 12:28:39 +0000

    Replace original sol with faster version and clean it up a bit more

--- 2022/14/sol.py
+++ 2022/14/sol.py
@@ -1,35 +1,37 @@
 from utils import *
 
 gr = Grid()
-lowest = defaultdict(lambda: -10000)
+floor = -100
 
 for line in inp_readlines():
     for p1, p2 in windows(chunks(ints_in(line), 2), 2):
         for p in Rectangle(p1, p2):
             gr[p] = "#"
-            lowest[p.x] = max(lowest[p.x], p.y)
+            floor = max(floor, p.y)
 
 sand_spawner = IVec2(500, 0)
 
-floor = max(lowest.values())+2
+floor += 2
 
 p1 = False
 
 num_sand = 0
-sand = sand_spawner
-while True:
-    if not p1 and sand.y > lowest[sand.x]:
+# Each grain of sand follows the same path as the previous one, except for the last point.
+# So we keep track of the path rather than start from the spawner each time.
+path = [sand_spawner]
+while path:
+    sand = path[-1]
+    if not p1 and sand.y+1 == floor:
         print("Part 1:", num_sand)
         p1 = True
     pts = [sand+(0, 1), sand+(-1, 1), sand+(1, 1)]
     for p in pts:
-        if p.y != floor and gr[p] not in ["#", "o"]:
-            sand = p
+        if p.y != floor and p not in gr:
+            path.append(p)
             break
     else:
         gr[sand] = "o"
         num_sand += 1
-        if sand == sand_spawner:
-            print("Part 2:", num_sand)
-            exit()
-        sand = sand_spawner
+        path.pop()
+
+print("Part 2:", num_sand)

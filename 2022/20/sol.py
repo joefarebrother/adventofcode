bea849e5586dc6dc3cf8a34db7f967e485e6d888
Joe Farebrother <joseph.farebrother@gmail.com>
2022-12-20 15:36:44 +0000

    Switch sol with sol_simple

--- 2022/20/sol.py
+++ 2022/20/sol.py
@@ -1,80 +1,36 @@
 from utils import *
 
 inp = ints(inp_readlines())
-print(len(set(inp)), len(inp))
+
+xs = list(enumerate(inp))
 
 
-class Node:
-    def __init__(self, val):
-        self.next = None
-        self.prev = None
-        self.val = val
-
-    def unlink(self):
-        pr = self.prev
-        nx = self.next
-        assert pr.next == self and nx.prev == self
-        pr.next = nx
-        nx.prev = pr
-        self.next = None
-        self.prev = None
-
-    def link(self, pr, nx):
-        self.unlink()
-        assert pr.next == nx and nx.prev == pr
-        self.next = nx
-        nx.prev = self
-        self.prev = pr
-        pr.next = self
-
-    def move_forward(self):
-        self.link(self.next, self.next.next)
-
-    def move_backward(self):
-        self.link(self.prev.prev, self.prev)
-
-    def move(self):
-        if self.val < 0:
-            for _ in range(-self.val % (len(nodes)-1)):
-                self.move_backward()
-        else:
-            for _ in range(self.val % (len(nodes)-1)):
-                self.move_forward()
-
-    def get(self, n):
-        x = self
-        for _ in range(n):
-            x = x.next
-        return x
+def move(xs: list, idx):
+    nidx = next(i for i, (j, x) in enumerate(xs) if j == idx)
+    (j, v) = xs.pop(nidx)
+    assert j == idx
+    nidx = (nidx + v) % len(xs)
+    xs.insert(nidx, (j, v))
+    return xs
 
 
-nodes = [Node(x) for x in inp]
-zero = only(n for n in nodes if n.val == 0)
-
-for x, y in windows(nodes+[nodes[0]], 2):
-    x.next = y
-    y.prev = x
+def mix(xs):
+    for i in range(len(xs)):
+        xs = move(xs, i)
+    return xs
 
 
-def mix():
-    for n in nodes:
-        n.move()
+def res(xs):
+    zidx = next(i for i, (j, x) in enumerate(xs) if x == 0)
+    idxs = [(zidx+k*1000) % len(xs) for k in irange(3)]
+    return sum(xs[idx][1] for idx in idxs)
 
 
-mix()
+print("Part 1:", res(mix(xs)))
 
-res = sum(zero.get(i*1000).val for i in range(4))
-print("Part 1:", res)
-
-for x, y in windows(nodes+[nodes[0]], 2):
-    x.next = y
-    y.prev = x
-
-for n in nodes:
-    n.val *= 811589153
+xs = [(i, 811589153*x)for i, x in enumerate(inp)]
 
 for _ in range(10):
-    mix()
+    xs = mix(xs)
 
-res = sum(zero.get(i*1000).val for i in range(4))
-print("Part 2:", res)
+print("Part 2:", res(xs))

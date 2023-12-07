a612c421343f81cb5c022dac1f4b41e113a167cf
Joe Farebrother <joseph.farebrother@gmail.com>
2023-12-07 05:42:42 +0000

    Day 7 cleanup

--- 2023/7/sol.py
+++ 2023/7/sol.py
@@ -4,6 +4,9 @@ ranks = "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")
 
 new_ranks = "A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J".split(", ")
 
+def parse(line):
+    hand, bid = line.split()
+    return (hand, int(bid))
 
 def kind(h):
     c = Counter(h)
@@ -22,34 +25,22 @@ def kind(h):
         return 5
     return 4
 
-class Hand:
-    def __init__(self, line):
-        hand, bid = line.split()
-        self.bid = int(bid)
-        self.hand = hand 
+def key(h):
+    return kind(h), mapl(lambda x: 10-ranks.index(x), h)
 
-    def kind(self):
-        return kind(self.hand)
-    
-    def key(self):
-        return self.kind(), mapl(lambda x: 10-ranks.index(x), self.hand)
-    
-    def kind_with_jokers(self):
-        h = self.hand
-        return max(kind(h.replace("J", c)) for c in new_ranks[:-1])
-    
-    def keyj(self):
-        return self.kind_with_jokers(), mapl(lambda x: 10-new_ranks.index(x), self.hand)
-    
-    def __repr__(self):
-        return f"{self.hand} {self.bid}"
-    
-hands = [Hand("A 0")] + sorted(mapl(Hand, inp_readlines()), key=Hand.key)
-print(hands)
+def kindj(h):
+    return max(kind(h.replace("J", c)) for c in new_ranks[:-1])
 
-print("Part 1:", sum(h.bid*i for i,h in enumerate(hands)))
+def keyj(h):
+    return kindj(h), mapl(lambda x: 10-new_ranks.index(x), h)
+    
 
-hands = [Hand("A 0")] + sorted(mapl(Hand, inp_readlines()), key=Hand.keyj)
-print(hands)
 
-print("Part 2:", sum(h.bid*i for i,h in enumerate(hands)))
+raw_hands = mapl(parse, inp_readlines())
+hands = [(0,0)] + sorted(raw_hands, key=lambda hb: key(hb[0]))
+
+print("Part 1:", sum(hb[1]*i for i,hb in enumerate(hands)))
+
+hands = [(0,0)] + sorted(raw_hands, key=lambda hb: keyj(hb[0]))
+
+print("Part 2:", sum(hb[1]*i for i,hb in enumerate(hands)))

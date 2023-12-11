032582da7ea1dacb2e282390fd9a2b96d73f5674
Joe Farebrother <joseph.farebrother@gmail.com>
2023-12-11 05:17:05 +0000

    Minor refactor day 11

--- 2023/11/sol.py
+++ 2023/11/sol.py
@@ -14,12 +14,15 @@ for p in g:
 expanded_rows = set(g.bounding_box.yrange()) - rows
 expanded_cols = set(g.bounding_box.xrange()) - cols
 
-adjusted_gals = set()
-for p in gals:
-    x,y = p 
-    nx = x + ((100 if is_ex else 1000000)-1)* len([ex for ex in expanded_cols if ex < x])
-    ny = y + ((100 if is_ex else 1000000)-1)* len([ey for ey in expanded_rows if ey < y])
-    adjusted_gals.add(IVec2(nx,ny))
+def tot_dist(expansion_rate):
+    adjusted_gals = set()
+    for p in gals:
+        x,y = p 
+        nx = x + (expansion_rate-1)* len([ex for ex in expanded_cols if ex < x])
+        ny = y + (expansion_rate-1)* len([ey for ey in expanded_rows if ey < y])
+        adjusted_gals.add(IVec2(nx,ny))
 
-tot_dist = sum(man_dist(p1, p2) for p1,p2 in itertools.product(adjusted_gals, adjusted_gals) if p1!=p2)
-print("Part 2:", tot_dist//2)
+    return sum(man_dist(p1, p2) for p1,p2 in itertools.product(adjusted_gals, adjusted_gals))//2
+    
+print("Part 1:", tot_dist(2))
+print("Part 2:", tot_dist(100 if is_ex else 1000000))

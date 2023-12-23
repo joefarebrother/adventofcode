06104b20e1a332a0cdbb725907bac905dfe75d4b
Joe Farebrother <joseph.farebrother@gmail.com>
2023-12-23 06:22:45 +0000

    Day 23

---
+++ 2023/23/sol.py
@@ -0,0 +1,72 @@
+from utils import *
+
+g = Grid(0)
+Dirs.flipy()
+
+def adj(p):
+    res = []
+    for np in neighbours(p):
+        if np not in g or g[np] == "#":
+            continue 
+        # if g[p] in "^v<>" and np-p != Dirs[g[p]]:
+        #     continue 
+        res.append(np)
+    return res 
+
+@cache
+def adj_choice(p):
+    res = {}
+    for np in adj(p):
+        d = 1
+        use=True
+        op = p
+        while len(anp := adj(np)) <= 2:
+            anp = set(anp)-{op}
+            if len(anp) == 0:
+                use = (np.y==g.height-1)
+                break 
+            d += 1
+            np,op = only(anp),np
+        if use:
+            res[np] = d 
+    return res
+
+@cache
+def longest(p, seen):
+    if p.y == g.height-1:
+        return 0
+    
+    if not goal_reachable(p,seen):
+        return -math.inf
+    
+    best = -math.inf
+    for np,d in adj_choice(p).items():
+        if np not in seen:
+            nseen = seen|{p} 
+            best = max(best,d+longest(np,nseen))
+            print(p,np,len(seen),d,best)
+    return best
+
+@cache
+def goal_reachable(p,seen):
+    def adj_mod(p):
+        return [np for np in adj_choice(p) if np not in seen]
+    return FGraph(adj_mod).DFS(p).dist(lambda np: np.y == g.height-1) != math.inf 
+
+
+
+import graphviz
+choice_nodes = set(FGraph(adj_choice).BFS(IVec2(1,0)).all_dists().keys())
+
+dot = graphviz.Digraph("gr")
+for p in choice_nodes:
+    dot.node(str(p))
+
+for p in choice_nodes:
+    for np,d in adj_choice(p).items():
+        if str(p) < str(np):
+            dot.edge(str(p),str(np),str(d))
+
+dot.render("2023/23/gr.gv",format="png")
+
+print("Part 2:", longest(IVec2(1,0),frozenset()))
